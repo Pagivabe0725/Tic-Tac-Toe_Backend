0138fe907f0c48fd7d9c08728621db44ce4eaa9f
Kovács <kovpatrik0725@gmail.com>
2025-10-30 15:31:50 +0100

    feat(validators, controllers): add input validation and update controller logic

---
+++ controllers/game.controller.js
@@ -0,0 +1,11 @@
+/* 
+{
+    board: string[][],
+    size:number,
+    step:number,
+    markup: 'x' | 'o',
+    hardness: 'very-easy' | 'easy' | 'medium' | 'hard',
+    lastMove?: {xCoordinate : number , yCoordinate:number},
+    winner: 'x' | 'o' | 'draw' | null
+}
+*/
